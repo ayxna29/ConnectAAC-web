5137a7ac5477c5b82f3d782e4ac04c8305bef11a
Ayana Singh <felixkookies@gmail.com>
2026-03-11 17:57:18 -0400

    add shared_preferences, fitz key settings

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4196046440"
+    serviceWorkerVersion: "3353207141"
   }
 });
