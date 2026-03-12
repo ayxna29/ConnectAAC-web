4f5f446c11bd6cd1fb03926bd955b1fffe2d9c31
Ayana Singh <felixkookies@gmail.com>
2026-03-11 23:05:58 -0400

    profile section, blank card fix, square cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2298461576"
+    serviceWorkerVersion: "2842872901"
   }
 });
