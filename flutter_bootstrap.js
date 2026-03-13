11e1db46bfeda89a4f327b0baa70112ca51e29c1
Ayana Singh <felixkookies@gmail.com>
2026-03-13 18:14:11 -0400

    fix web mic permission

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2842872901"
+    serviceWorkerVersion: "2382091740"
   }
 });
