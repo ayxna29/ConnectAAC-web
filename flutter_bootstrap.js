80e0a8329d2251be19efb8c002deaeaed7837161
Ayana Singh <felixkookies@gmail.com>
2026-03-15 12:22:14 -0400

    update symbols

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "357018673"
+    serviceWorkerVersion: "4195421083"
   }
 });
